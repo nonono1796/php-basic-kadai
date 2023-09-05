bb5f4bd479023840329512471e729950fc33c4e1
nozawamitsuki <kaminagara13@gmail.com>
2023-09-05 22:10:44 +0900

    kadai_007の提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+<p>
+    <?php
+    // 連想配列に値を代入する
+    $yasai=['name'=>'onion','price'=>200,'weigh'=>160];
+    // 連想配列の値を出力する
+    print_r($yasai);
+    ?>
+</p>
+</html>
